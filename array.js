e34410738346285723f87ac38ede7d88b77dea68
Rajib Ahmed <rajibdpi@gmail.com>
2021-02-22 23:08:55 +0600

    Updated codes

---
+++ array.js
@@ -0,0 +1,6 @@
+var bag = ["Chal", "Dal", "Ata", "Saban", "Apple", "Orange", "Plum", "1BHK", "2BHK", "3BHK", "4BHK"];
+
+for (var i = 0; i < bag.length; i++) {
+    const x = bag[i];
+    console.log(`Item is ${i + '-->' + x}`);
+}
